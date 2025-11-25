3c630c73d03296cf2c2f48915b0229d1d63411ab
Taha Ramadan <tmr07@mail.aub.edu>
2025-11-26 01:15:01 +0200

    Add files via upload

---
+++ mafiabackend/backendmafia.py
@@ -0,0 +1,37 @@
+# Install dependencies first:
+# pip install fastapi uvicorn
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+# Keep track of connected clients
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Broadcast received message to all clients
+            await manager.broadcast(f"Player says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast("A player disconnected")
